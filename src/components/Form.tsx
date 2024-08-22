0b65a9fa11cb95706cd86a5ec5a683f9dc5a38c4
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-08-21 22:15:11 -0500

    add: agregar edicion de calorias

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,12 @@
-import { useState, ChangeEvent, FormEvent, Dispatch } from "react"
+import { useState, ChangeEvent, FormEvent, Dispatch, useEffect } from "react"
 import { v4 as uuidv4 } from "uuid"
 import { categories } from "../data/categories"
 import { Activity } from "../types"
-import { ActivityActions } from "../reducers/activity-reducer"
+import { ActivityActions, ActivityState } from "../reducers/activity-reducer"
 
 type FormProps = {
-    dispatch: Dispatch<ActivityActions>
+    dispatch: Dispatch<ActivityActions>,
+    state: ActivityState
 }
 
 const initialStage: Activity = {
@@ -15,9 +16,17 @@ const initialStage: Activity = {
     calories: 0
 }
 
-export const Form = ({ dispatch }: FormProps) => {
+export const Form = ({ dispatch, state }: FormProps) => {
 
     const [activity, setActivity] = useState<Activity>(initialStage)
+
+    useEffect(() => {
+        if (state.activeId) {
+            console.log("activeId", state.activeId)
+            const selectedActivity = state.activities.filter(stateActivity => stateActivity.id === state.activeId)[0]
+            setActivity(selectedActivity)
+        }
+    }, [state.activeId])
 
     const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
         const isNumberField = ['category', 'calories'].includes(e.target.id)
